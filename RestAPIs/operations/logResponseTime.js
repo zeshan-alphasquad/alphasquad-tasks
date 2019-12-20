d6a26d238efd9ef5c68c3fb428ff53169bfd5b30
zeshan-alphasquad <zeshan.alphasquad@gmail.com>
2019-12-20 15:49:05 +0500

    Logging Response time in text file

--- RestAPIs/operations/logResponseTime.js
+++ RestAPIs/operations/logResponseTime.js
@@ -1,10 +1,20 @@
 // response-time-logger.js
+var fs = require('fs')
 function logResponseTime(req, res, next) {
+    // Incomming request time 
+    const now = new Date()
     const startHrTime = process.hrtime();
   
     res.on("finish", () => {
+      // Getting final response time
       const elapsedHrTime = process.hrtime(startHrTime)
       const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6
+      var timeLog = now+' - '+req.path+' : '+elapsedTimeInMs+'\n'
+      // Logging time in file
+      fs.appendFile('responseTimeLog.txt',  timeLog, (err) => {
+        if (err) throw new Error('Couldn\'t write the data to a file');
+        // console.log('Time Logged in file');
+        });
       console.log("%s : %fms", req.path, elapsedTimeInMs)
     })
     next()
